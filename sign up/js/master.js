a321a9f792af3a2725630f8efa737b4683a87ad3
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2024-07-06 02:29:45 +0300

    third commit

--- sign up/js/master.js
+++ sign up/js/master.js
@@ -1,39 +1,38 @@
-var signName = document.getElementById('signName')
-var signEmail = document.getElementById('signEmail')
-var signPassword = document.getElementById('signPassword')
-var user = []
-if (localStorage.getItem('user') != null) {
-    user = JSON.parse(localStorage.getItem('user'))
+var signName = document.getElementById('signName');
+var signEmail = document.getElementById('signEmail');
+var signPassword = document.getElementById('signPassword');
+
+var user = [];
+if (localStorage.getItem('user') !== null) {
+    user = JSON.parse(localStorage.getItem('user'));
 }
-else {
-    user = []
-}
-
-
-
-
-
 
 function add() {
-    if (signName.value == '' || signEmail.value == '' || signPassword.value == '') {
-        document.getElementById('message').innerHTML = `<p class="text-center text-danger">All inputs is required</p>`
+    var userEmail = signEmail.value.toLowerCase(); 
+   
+    var isEmailExist = user.some(function(item) {
+        return item.email.toLowerCase() === userEmail;
+    });
 
+    if (signName.value === '' || userEmail === '' || signPassword.value === '') {
+        document.getElementById('message').innerHTML = `<p class="text-center text-danger">All inputs are required</p>`;
+        document.getElementById('message2').innerHTML = ''; 
+        document.getElementById('message4').innerHTML = '';
+    }
+    else if (isEmailExist) {
+        document.getElementById('message4').innerHTML = `<p class="text-center text-danger">Email already exists</p>`;
+        document.getElementById('message2').innerHTML = ''; 
     }
     else {
-        document.getElementById('message2').innerHTML = `<p class="text-center text-success">Success</p>`
-        document.getElementById('message').classList.add('d-none')
-        var obj =
-        {
+        document.getElementById('message2').innerHTML = `<p class="text-center text-success">Success</p>`;
+        document.getElementById('message').classList.add('d-none');
+        document.getElementById('message4').innerHTML = '';
+        var obj = {
             name: signName.value,
-            email: signEmail.value,
+            email: userEmail, 
             password: signPassword.value
-        }
-        user.push(obj)
-        // location.href = '../login/index.html'
-
+        };
+        user.push(obj);
         localStorage.setItem('user', JSON.stringify(user));
     }
 }
-    
-               
-     
